78b74a8f8e5628c106d1e8124e66640c95387bbb
TanyaFox <tanya-smile1994@mail.ru>
2016-11-18 01:01:16 +0400

    ContextClassWasAdded

---
+++ Data/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    class Context : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+
+        public Context() : base("Sports")
+        {
+
+        }
+    }
+}
